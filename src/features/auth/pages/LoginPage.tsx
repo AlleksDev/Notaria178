37652690633674d01c3fba890ce6e8e47412f0b6
JosephAntonyDev <joseantoniopintoaguilar@gmail.com>
2026-03-04 11:08:26 -0600

    add: files index.tx and update .gitignore

---
+++ src/features/auth/pages/LoginPage.tsx
@@ -0,0 +1,15 @@
+// src/features/auth/pages/LoginPage.tsx
+export const LoginPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md w-96">
+        <h1 className="text-2xl font-bold text-center text-blue-900 mb-6">
+          Notaría 178
+        </h1>
+        <p className="text-center text-gray-600">
+          Formulario de Login en construcción...
+        </p>
+      </div>
+    </div>
+  );
+};
